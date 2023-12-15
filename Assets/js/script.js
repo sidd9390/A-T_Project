5ad6edba082cb2340632d2e8600a2aa2992f2807
Grandhi Siddu <113771456+sidd9390@users.noreply.github.com>
2023-12-15 16:39:48 +0530

    new file:   Assets/Img/fav-icon.png 	modified:   Assets/css/Home.css 	modified:   Assets/css/style.css 	new file:   Assets/js/script.js 	modified:   Blogs.html 	modified:   Carrers.html 	modified:   Contact_Us.html 	modified:   Distribution.html 	modified:   Home.html 	modified:   Knowledge-center.html 	modified:   Media.html 	modified:   Product.html 	modified:   Services_consultancy.html 	modified:   index.html

---
+++ Assets/js/script.js
@@ -0,0 +1,115 @@
+let frame = document.getElementById("my_frame");
+frame.addEventListener('scroll', changeBg());
+function changeBg()
+{
+  var navbar = document.getElementById('border');
+  var scrollValue = frame.scrollY;
+  console.log(scrollValue);
+  if(scrollValue < 70){
+    navbar.classList.remove('bor');
+  }
+  else{
+    navbar.classList.add('bor');
+  }
+  
+}
+
+function b1(){
+    document.getElementById("btn-1").style.borderBottomColor = "#00a759";
+    document.getElementById("btn-2").style.borderBottomColor = "transparent";
+    document.getElementById("btn-3").style.borderBottomColor = "transparent";
+    document.getElementById("btn-4").style.borderBottomColor = "transparent";
+    document.getElementById("btn-5").style.borderBottomColor = "transparent";
+    document.getElementById("btn-6").style.borderBottomColor = "transparent";
+    document.getElementById("btn-7").style.borderBottomColor = "transparent";
+    document.getElementById("btn-8").style.borderBottomColor = "transparent";
+    document.getElementById("btn-9").style.borderBottomColor = "transparent";
+}
+function b2(){
+    document.getElementById("btn-1").style.borderBottomColor = "transparent";
+    document.getElementById("btn-2").style.borderBottomColor = "#00a759";
+    document.getElementById("btn-3").style.borderBottomColor = "transparent";
+    document.getElementById("btn-4").style.borderBottomColor = "transparent";
+    document.getElementById("btn-5").style.borderBottomColor = "transparent";
+    document.getElementById("btn-6").style.borderBottomColor = "transparent";
+    document.getElementById("btn-7").style.borderBottomColor = "transparent";
+    document.getElementById("btn-8").style.borderBottomColor = "transparent";
+    document.getElementById("btn-9").style.borderBottomColor = "transparent";
+}
+function b3(){
+    document.getElementById("btn-1").style.borderBottomColor = "transparent";
+    document.getElementById("btn-2").style.borderBottomColor = "transparent";
+    document.getElementById("btn-3").style.borderBottomColor = "#00a759";
+    document.getElementById("btn-4").style.borderBottomColor = "transparent";
+    document.getElementById("btn-5").style.borderBottomColor = "transparent";
+    document.getElementById("btn-6").style.borderBottomColor = "transparent";
+    document.getElementById("btn-7").style.borderBottomColor = "transparent";
+    document.getElementById("btn-8").style.borderBottomColor = "transparent";
+    document.getElementById("btn-9").style.borderBottomColor = "transparent";
+}
+function b4(){
+    document.getElementById("btn-1").style.borderBottomColor = "transparent";
+    document.getElementById("btn-2").style.borderBottomColor = "transparent";
+    document.getElementById("btn-3").style.borderBottomColor = "transparent";
+    document.getElementById("btn-4").style.borderBottomColor = "#00a759";
+    document.getElementById("btn-5").style.borderBottomColor = "transparent";
+    document.getElementById("btn-6").style.borderBottomColor = "transparent";
+    document.getElementById("btn-7").style.borderBottomColor = "transparent";
+    document.getElementById("btn-8").style.borderBottomColor = "transparent";
+    document.getElementById("btn-9").style.borderBottomColor = "transparent";
+}
+function b5(){
+    document.getElementById("btn-1").style.borderBottomColor = "transparent";
+    document.getElementById("btn-2").style.borderBottomColor = "transparent";
+    document.getElementById("btn-3").style.borderBottomColor = "transparent";
+    document.getElementById("btn-4").style.borderBottomColor = "transparent";
+    document.getElementById("btn-5").style.borderBottomColor = "#00a759";
+    document.getElementById("btn-6").style.borderBottomColor = "transparent";
+    document.getElementById("btn-7").style.borderBottomColor = "transparent";
+    document.getElementById("btn-8").style.borderBottomColor = "transparent";
+    document.getElementById("btn-9").style.borderBottomColor = "transparent";
+}
+function b6(){
+    document.getElementById("btn-1").style.borderBottomColor = "transparent";
+    document.getElementById("btn-2").style.borderBottomColor = "transparent";
+    document.getElementById("btn-3").style.borderBottomColor = "transparent";
+    document.getElementById("btn-4").style.borderBottomColor = "transparent";
+    document.getElementById("btn-5").style.borderBottomColor = "transparent";
+    document.getElementById("btn-6").style.borderBottomColor = "#00a759";
+    document.getElementById("btn-7").style.borderBottomColor = "transparent";
+    document.getElementById("btn-8").style.borderBottomColor = "transparent";
+    document.getElementById("btn-9").style.borderBottomColor = "transparent";
+}
+function b7(){
+    document.getElementById("btn-1").style.borderBottomColor = "transparent";
+    document.getElementById("btn-2").style.borderBottomColor = "transparent";
+    document.getElementById("btn-3").style.borderBottomColor = "transparent";
+    document.getElementById("btn-4").style.borderBottomColor = "transparent";
+    document.getElementById("btn-5").style.borderBottomColor = "transparent";
+    document.getElementById("btn-6").style.borderBottomColor = "transparent";
+    document.getElementById("btn-7").style.borderBottomColor = "#00a759";
+    document.getElementById("btn-8").style.borderBottomColor = "transparent";
+    document.getElementById("btn-9").style.borderBottomColor = "transparent";
+}
+function b8(){
+    document.getElementById("btn-1").style.borderBottomColor = "transparent";
+    document.getElementById("btn-2").style.borderBottomColor = "transparent";
+    document.getElementById("btn-3").style.borderBottomColor = "transparent";
+    document.getElementById("btn-4").style.borderBottomColor = "transparent";
+    document.getElementById("btn-5").style.borderBottomColor = "transparent";
+    document.getElementById("btn-6").style.borderBottomColor = "transparent";
+    document.getElementById("btn-7").style.borderBottomColor = "transparent";
+    document.getElementById("btn-8").style.borderBottomColor = "#00a759";
+    document.getElementById("btn-9").style.borderBottomColor = "transparent";
+}
+function b9(){
+    document.getElementById("btn-1").style.borderBottomColor = "transparent";
+    document.getElementById("btn-2").style.borderBottomColor = "transparent";
+    document.getElementById("btn-3").style.borderBottomColor = "transparent";
+    document.getElementById("btn-4").style.borderBottomColor = "transparent";
+    document.getElementById("btn-5").style.borderBottomColor = "transparent";
+    document.getElementById("btn-6").style.borderBottomColor = "transparent";
+    document.getElementById("btn-7").style.borderBottomColor = "transparent";
+    document.getElementById("btn-8").style.borderBottomColor = "transparent";
+    document.getElementById("btn-9").style.borderBottomColor = "#00a759";
+}
